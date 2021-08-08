03df878ea5d80cad35ca4ec9c80f57c4dad9ea2c
Gabriela Sillis <gabydias47@gmail.com>
2021-08-08 18:24:17 -0300

    Weather ViewController refactoring

--- weather/weather/controller/WeatherController.swift
+++ weather/weather/controller/WeatherController.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import  CoreLocation
+
+protocol  WeatherControllerDelegate: AnyObject {
+    func updateView(with data: WeatherModel)
+    func handleError(_ error: Error)
+}
+
+class WeatherController {
+
+    private let weatherNetwork: WeatherNetwork = WeatherNetwork()
+    private let locationController: LocationController = LocationController()
+
+    weak var delegate: WeatherControllerDelegate?
+
+
+    func fetchWeather(by city: String) {
+        self.weatherNetwork.fetchWeatherByCity(city: city) { result in
+            self.handleResult(result)
+        }
+    }
+
+    func fetchWeatherByLocation(location: CLLocation) {
+        let lati = location.coordinate.latitude
+        let long = location.coordinate.longitude
+        self.weatherNetwork.featchWeatherByLocation(lati: lati, long: long) { result in
+
+            // espera para atualizar a view - assim é possível ver o skeletonview
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+                self.handleResult(result)
+            }
+        }
+    }
+
+    private func handleResult(_ result: Result<WeatherModel, Error>) {
+        switch result {
+        case.success(let model):
+            self.delegate?.updateView(with: model)
+
+        case.failure(let error):
+            self.delegate?.handleError(error)
+        }
+    }
+}
--- weather/weather/view/WeatherViewController.swift
+++ weather/weather/view/WeatherViewController.swift
@@ -10,6 +10,7 @@ import SkeletonView
 import CoreLocation
 
 class WeatherViewController: UIViewController {
+    
 
     // MARK: - IBOutlet
     @IBOutlet weak var weatherForecastImage: UIImageView!
@@ -21,16 +22,17 @@ class WeatherViewController: UIViewController {
     
     
     // MARK: - Properties
-    private let weatherNetwork: WeatherNetwork = WeatherNetwork()
+    private let weatherController: WeatherController = WeatherController()
     private let locationController: LocationController = LocationController()
     
     // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.fetchWeather(by: "São Paulo")
         self.locationController.delegate = self
+        self.weatherController.delegate = self
+        self.showAnimatedSkeleton()
+        self.weatherController.fetchWeather(by: "São Paulo")
     }
-
     // MARK: - IBAction
     @IBAction func locationButtonTapped(_ sender: UIButton) {
         self.locationController.requestLocationUsage()
@@ -38,50 +40,6 @@ class WeatherViewController: UIViewController {
     
     @IBAction func addCityButtonTapped(_ sender: UIButton) {
         performSegue(withIdentifier: "showAddCity", sender: nil)
-    }
-    
-    // MARK: - fetchWeather by city
-    private func fetchWeather(by city: String) {
-        self.showAnimatedSkeleton()
-        self.weatherNetwork.fetchWeatherByCity(city: city) { result in
-            self.handleResult(result)
-        }
-    }
-    
-    // MARK: - fetchWeather by location
-
-    // Passar para o controller
-    private func fetchWeatherByLocation(location: CLLocation) {
-        showAnimatedSkeleton()
-        let lati = location.coordinate.latitude
-        let long = location.coordinate.longitude
-        self.weatherNetwork.featchWeatherByLocation(lati: lati, long: long) { result in
-            
-            // espera para atualizar a view - assim é possível ver o skeletonview
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                self.handleResult(result)
-            }
-        }
-    }
-    
-    // MARK: - handleResult
-    private func handleResult(_ result: Result<WeatherModel, Error>) {
-        switch result {
-        case.success(let model):
-                updateView(with: model)
-        case.failure(let error):
-                handleError(error)
-        }
-    }
-    
-    // MARK: - handleError
-    private func handleError(_ error: Error) {
-        self.hideAnimation()
-        self.weatherForecastImage.image = UIImage(named: "imSad")
-        self.temperatureLabel.text = "Ooops!"
-        self.conditionLabel.text = "Algo deu errado, tente novamente."
-        self.cityNameLabel.text = ""
-        self.backgroundImage.image = UIImage(named: "Rain")
     }
     
     // MARK: - Skeleton Animation
@@ -101,19 +59,6 @@ class WeatherViewController: UIViewController {
         self.backgroundImage.hideSkeleton()
     }
     
-    // MARK: - updateView
-    private func updateView(with data: WeatherModel) {
-        // atualiza as labels e imagem da view e pausa a animação
-        self.hideAnimation()
-        self.temperatureLabel.text = data.temp.toString().appending("°C")
-        self.weatherForecastImage.image = UIImage(named: data.conditionImage)
-        self.conditionLabel.text = data.conditionDescription
-        self.cityNameLabel.text = data.cityName
-        self.backgroundImage.image = UIImage(named: data.conditionBackground)
-    }
-
-    
-    
     // MARK: - perfom segue
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "showAddCity" {
@@ -124,7 +69,7 @@ class WeatherViewController: UIViewController {
     }
 }
 
-// MARK: - Protocol
+// MARK: - Protocol AddCityViewControllerDelegate
 extension WeatherViewController: AddCityViewControllerDelegate {
     func didUpdateViewFromSearch(model: WeatherModel) {
         // dismiss no addCityVc e update da weatherVc
@@ -135,14 +80,35 @@ extension WeatherViewController: AddCityViewControllerDelegate {
     }
 }
 
-// MARK: - CLLocationManagerDelegate
+// MARK: - Protocol LocationControllerDelegate
 extension WeatherViewController: LocationControllerDelegate {
 
     func locationManager(didUpdateLocations locations: CLLocation) {
-        self.fetchWeatherByLocation(location: locations)
+        self.weatherController.fetchWeatherByLocation(location: locations)
     }
 
     func locationManager(didFailWithError error: Error) {
         self.handleError(error)
+    }
+}
+
+// MARK: - Protocol WeatherControllerDelegate
+extension WeatherViewController: WeatherControllerDelegate {
+    func updateView(with data: WeatherModel) {
+        self.hideAnimation()
+        self.temperatureLabel.text = data.temp.toString().appending("°C")
+        self.weatherForecastImage.image = UIImage(named: data.conditionImage)
+        self.conditionLabel.text = data.conditionDescription
+        self.cityNameLabel.text = data.cityName
+        self.backgroundImage.image = UIImage(named: data.conditionBackground)
+    }
+
+    func handleError(_ error: Error) {
+        self.hideAnimation()
+        self.weatherForecastImage.image = UIImage(named: "imSad")
+        self.temperatureLabel.text = "Ooops!"
+        self.conditionLabel.text = "Algo deu errado, tente novamente."
+        self.cityNameLabel.text = ""
+        self.backgroundImage.image = UIImage(named: "Rain")
     }
 }
